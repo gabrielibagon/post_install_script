d6519d3ea40049967631bce808f927daa2e64737
gabrielibagon <gabriel.ibagon@gmail.com>
2016-02-05 12:30:34 -0500

    requirements.txt used for python sdk

--- post_install_script.sh
+++ post_install_script.sh
@@ -54,17 +54,19 @@ sudo wget http://openbci.com//apps/application.linux64.zip
 unzip application.linux64.zip
 sudo chmod 775 /application.linux64.zip/OpenBCI_GUI
 
-##install the python API and dependencies
-# check if pip is installed, if not -> install
-sudo command -v pip >/dev/null 2>&1 || {}
-sudo apt-get -y install python-setuptools python-dev build-essential 
+###install the python API and dependencies
 
+## check if pip is installed, if not -> install
+# get build essentials
+sudo apt-get -y install python-setuptools python-dev build-essential 
+# if pip is not installed, instal pip
 if [ $(sudo dpkg-query -W -f='${Status}' pip 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
 	sudo easy_install pip;
 fi
 
 sudo git clone https://github.com/OpenBCI/OpenBCI_Python.git
+pip install -r OpenBCI_Python/requirements.txt 
 
 
 # list of apps:
