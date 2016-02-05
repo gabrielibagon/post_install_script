5686be3cddeb6dfc9f369ef5e3b9d922705d084d
gabrielibagon <gabriel.ibagon@gmail.com>
2016-02-05 16:12:56 -0500

    Update openbci_script.sh

--- openbci_script.sh
+++ openbci_script.sh
@@ -2,7 +2,7 @@
 mkdir ~/OpenBCI
 #install the GUI
 sudo wget -P ~/OpenBCI http://openbci.com//apps/application.linux64.zip
-unzip application.linux64.zip
+unzip ~/OpenBCI/application.linux64.zip
 sudo chmod 775 ~/OpenBCI/application.linux64.zip/OpenBCI_GUI
 
 ###install the python API and dependencies
@@ -15,7 +15,6 @@ if [ $(sudo dpkg-query -W -f='${Status}' pip 2>/dev/null | grep -c "ok installed
 then
 	sudo easy_install pip;
 fi
-
 sudo git clone https://github.com/OpenBCI/OpenBCI_Python.git
 sudo sed "1d" OpenBCI_Python/requirements.txt
 sudo pip install -r OpenBCI_Python/requirements.txt 
